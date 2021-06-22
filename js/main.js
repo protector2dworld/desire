266176b2f0a44410b8c4d414f6167a23a3ee7d69
protector2dworld <84408589+protector2dworld@users.noreply.github.com>
2021-06-22 19:43:16 +0300

    desire

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(function () {
+$('.header__btn').on('click', function () {
+ $('.rightside-menu').removeClass('rightside-menu--close');
+})      
+
+
+$('.rightside-menu__close').on('click', function () {
+    $('.rightside-menu').addClass('rightside-menu--close');
+});
+
+$('.top__slider').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true
+});
+
+$('.contact-slider').slick({
+    slidesToScroll: 10,
+    slidesToShow: 10,
+    dots: true,
+    arrows: false,
+});
+
+$('.article-slider__box').slick({
+    prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/blog/slider-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/blog/slider-right.svg" alt=""></button>'
+    
+});
+
+
+
+var mixer = mixitup('.gallery__inner', {
+    load:{
+        filter: '.living'
+    }
+});
+
+})
